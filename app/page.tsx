ab3233c2a3cf830b53163fe41fd6d679575111de
jhyunwoo <jhyunwoo0228@gmail.com>
2023-06-23 23:35:39 +0900

    add notification

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,21 @@ export default async function Home() {
       <BottomBar />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className=" bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition duration-200 flex flex-col justify-center items-start">
+          <div className="text-slate-600 text-xs">공지</div>
+          <div className="text-sm font-medium break-keep mt-1">
+            조화작에서 제공하는 모든 OX 퀴즈는{" "}
+            <span className="underline decoration-red-600 decoration-2 font-bold underline-offset-0">
+              조성민
+            </span>{" "}
+            학생이 출제한 것으로 답이 정확하지 않을 수 있습니다. 문제와 답안에
+            대해 <span className="text-sky-600 font-bold">비판적인 시각</span>
+            으로 봐주세요.{" "}
+            <span className="text-red-600">
+              답안 오류로 인한 모든 손해는 조화작에서 책임지지 않습니다.
+            </span>
+          </div>
+        </div>
         {chapters.map((data, key) => (
           <Link
             href={`/chapter/${data.id}`}
@@ -52,21 +67,21 @@ export default async function Home() {
             </div>
           </Link>
         ))}
-        {/* <Link
-          href={"https://yeonjunquiz.vercel.app/JungboTongsin/quiz"}
+        <Link
+          href={"https://inco.moveto.kr"}
           className=" bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition duration-200 flex flex-col justify-center items-start"
         >
           <div className="text-slate-600 text-xs">광고</div>
           <div className="text-base sm:text-lg font-medium break-keep mt-1">
             시작부터 끝까지,{" "}
             <span className="text-sky-600 font-bold">정보통신</span>은 누구나
-            김연준
+            INCO
           </div>
           <div className="text-xl sm:text-2xl font-semibold mt-1 break-keep pb-4">
-            2회고사도 <span className="text-sky-600 font-bold">김연준</span>만
-            믿고 따라 오세요.
+            2회고사도 <span className="text-sky-600 font-bold">INCO</span>로
+            대비하세요.
           </div>
-        </Link> */}
+        </Link>
       </div>
     </Layout>
   );
